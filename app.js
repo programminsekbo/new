13de5013a60c08453973fa8c79fbc772dc4fe21a
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-07-06 06:44:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
           "use strict";
-        class myClass{
-        myfun(name){
-          console.log(name)
+       class myClass{
+        constructor(){
 
-
-        }
-        myfun1(name){
-          console.log(name)
-
-
-        }
-        myfun2(name){
-          console.log(name)
-
-
-        }
-        myfun3(){
-          console.log("i love you ma")
-
-
-        }
-          
-
+          console.log("hello word")
 
         }
 
-        var objat =new myClass;
-        objat.myfun("hridoy")
-        objat.myfun1("dinajpur")
-        objat.myfun2("dhaka")
-        objat.myfun3()
-      
+
+
+
+
+       }
+       new myClass();
