221ec31acc03c8f91cf34966f7b0d3c6face0c65
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-07-05 23:21:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
   "use strict";
- var myFun = function(){
-
-  return "hello";
- }
-  
-
- console.log(myFun())
+var one = ["dinajpor","sethabgonj","rampur","thakurgonj"]
+var two = ["1","2","3","4"]
+var thre = ["a","b","c","d"]
+var mot = [one,two,thre]
+console.log(mot[2][3])
